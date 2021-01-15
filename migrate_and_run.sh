e7a6e4df2e863b37514258f48d14334c548b4148
hukumka <hukumka212@gmail.com>
2021-01-15 04:57:54 +0000

    Serve static files with nginx

--- migrate_and_run.sh
+++ migrate_and_run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-python manage.py makemigrations rest
-python manage.py migrate
+if [ ! -f "DJANGO_INITIALIZED" ]; then
+    python manage.py makemigrations rest \
+        && python manage.py migrate \
+        && python manage.py collectstatic --no-input --clear \
+        && touch DJANGO_INITIALIZED
+fi
 gunicorn sibdevrest.wsgi -b $1
